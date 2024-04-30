a98069e274229b5f4b0aae3a2e4479b71afc72ad
Alexander <69044271+TagGamer555@users.noreply.github.com>
2024-04-30 20:22:13 +0100

    yes

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -4,7 +4,11 @@
 	
 	//P.S: Chronological sorting. Newer content will appear after older content. Top - oldest in the category; Bottom - newest in the category.
 	
-	// ABLAZE FAMILY //
+	// BLAZE FAMILY //
 	// Family of turrets which ignite or melt targets by heat //
 	const candle = extend(ItemTurret, "candle", {});
+	
+	// FREEZE FAMILY //
+	// Family of turrets which slow down or freeze enemies via freezing efffects //
+	const freezeRay = extend(ItemTurret, "freeze-ray", {});
 }
